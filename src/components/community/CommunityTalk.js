c4c22b321539d6af599549af76d0c7d59a099d5d
ksy7615 <sykim7615@daum.net>
2024-05-29 00:06:56 +0900

    Add: Community List

---
+++ src/components/community/CommunityTalk.js
@@ -0,0 +1,55 @@
+import { Heading } from '@chakra-ui/layout';
+import { Table, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Box } from '@chakra-ui/react'
+
+const CommunityTalk = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/community?command=read/talk`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                setData(data);
+            })
+            .catch(error => {
+                console.error('데이터를 가져오는 중 에러 발생', error);
+            });
+    }, []);
+
+    return (
+        <>
+            <Box h='auto' w='100%'>
+                <Heading>
+                    잡담 커뮤니티
+                </Heading>
+
+            </Box>
+            <TableContainer>
+                <Table varient={"striped"} colorScheme="teal">
+                    <Thead>
+                        <Tr>
+                            <Th>제목</Th>
+                            <Th>작성자</Th>
+                            <Th>작성일</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {data.map((item, index) => (
+                            <Tr key={index}>
+                                <Td>{item.title}</Td>
+                                <Td>{item.userNickName}</Td>
+                                <Td>{item.regDate}</Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                    <Tfoot></Tfoot>
+                </Table>
+            </TableContainer>
+        </>
+    );
+};
+
+export default CommunityTalk;
