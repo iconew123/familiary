32823d32b6470c1539789e2f68a83597962ca8f1
yoon-wish <sowon5685@gmail.com>
2024-05-29 16:13:19 +0900

    Add 코드로 아기 등록하기

--- src/components/diaryHome/baby/JoinBabyByCode.js
+++ src/components/diaryHome/baby/JoinBabyByCode.js
@@ -1,80 +1,101 @@
-import { Box, Button, Input, Select, Text } from '@chakra-ui/react';
+import { Box, Button, Input, Select, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const JoinBabyByCode = () => {
-// 데이터 받아오기
-const [data, setData] = useState({});
-const navigate = useNavigate();
+    const [babyInfo, setBabyInfo] = useState({
+        user_id: '',
+        baby_code: '',
+        position: ''
+    });
 
-const [babyInfo, setBabyInfo] = useState({
-    user_id: '',
-    baby_code: '',
-    position: '',
-});
+    const navigate = useNavigate();
+    const { isOpen: isErrorModalOpen, onOpen: openErrorModal, onClose: closeErrorModal } = useDisclosure();
+    const [modalMessage, setModalMessage] = useState('');
 
-const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setBabyInfo(prevState => ({
-        ...prevState,
-        [name]: value
-    }));
-};
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setBabyInfo(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
+    const handleButtonClick = () => {
+        const formData = new FormData();
+        formData.append('user_id', 'hoit5it');
+        formData.append('position', babyInfo.position);
+        formData.append('baby_code', babyInfo.baby_code);
 
-
-const handleButtonClick = () => {
-
-    const formData = new FormData();
-    formData.append('user_id', babyInfo.user_id);
-    formData.append('baby_code', babyInfo.baby_code);
-    formData.append('position', babyInfo.position);
-
-    fetch(`${process.env.REACT_APP_SERVER_URL}/baby?command=enroll`, {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log('데이터 전송 성공');
-                navigate('/diary');
-            } else {
-                console.log('데이터 전송 실패')
-            }
+        fetch(`${process.env.REACT_APP_SERVER_URL}/baby?command=enroll`, {
+            method: 'POST',
+            body: formData
         })
-        .catch(error => {
-            console.error('데이터를 전송하는 중 에러 발생', error);
-        });
-};
+            .then(response => response.json())
+            .then(data => {
+                console.log('data : ' + data);
+                console.log('baby : ' + data.baby);
+                if(data.dupl){
+                    setModalMessage("이미 등록 중인 아기입니다.")
+                    openErrorModal();
+                }
+                else if (data.baby) {
+                    console.log('exist: ' + data.exists)
+                    if (data.exists) {
+                        setModalMessage("이미 존재하는 포지션입니다.");
+                        openErrorModal(); // 에러 모달 열기
+                    } else {
+                        navigate('/diary');
+                    }
+                } else {
+                    setModalMessage("존재하지 않는 코드입니다.");
+                    openErrorModal(); // 에러 모달 열기
+                } 
+            })
+            .catch(error => {
+                console.error('데이터를 전송하는 중 에러 발생', error);
+            });
+    };
 
+    return (
+        <>
+            <Box
+                bg='#fffbf0'
+                h='auto'
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                flexDirection='column'>
 
+                <Text fontSize='4xl' as='b' color='#765d2f' marginTop='30px' marginBottom='30px'>아기 등록하기</Text>
 
-return (
-    <>
-        <Box
-            bg='#fffbf0'
-            h='auto'
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            flexDirection='column'>
-
-
-            <Text fontSize='4xl' as='b' color='#765d2f' marginTop='30px' marginBottom='30px'>아기 등록하기</Text>
-            
-            <Input placeholder='아기코드' name='baby_code' value={babyInfo.baby_code} onChange={handleInputChange} size='lg' bg='white' w='500px' h='60px' />
-            <Select placeholder='아기와의 관계' bg='white' w='500px' h='60px' defaultValue={babyInfo.position}
-                 onChange={(e) => setBabyInfo({ ...babyInfo, position: e.target.value })}>
+                <Input placeholder='아기코드' name='baby_code' value={babyInfo.baby_code} onChange={handleInputChange} size='lg' bg='white' w='500px' h='60px' />
+                <Select placeholder='아기와의 관계' bg='white' w='500px' h='60px' defaultValue={babyInfo.position} onChange={(e) => setBabyInfo({ ...babyInfo, position: e.target.value })}>
                     <option value='mother'>엄마</option>
                     <option value='father'>아빠</option>
                     <option value='family'>가족</option>
                 </Select>
 
-            <Button onClick={handleButtonClick} w='100px' bg='#e0ccb3' marginTop='40px' _hover={{ color: '#fffbf0' }} marginBottom='50px'>등록하기</Button>
+                <Button onClick={handleButtonClick} w='100px' bg='#e0ccb3' marginTop='40px' _hover={{ color: '#fffbf0' }} marginBottom='50px'>등록하기</Button>
 
-        </Box>
-    </>
-);
+                {/* 에러 모달 */}
+                <Modal isOpen={isErrorModalOpen} onClose={closeErrorModal}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>에러</ModalHeader>
+                        <ModalBody>
+                            {modalMessage}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme="blue" mr={3} onClick={closeErrorModal}>
+                                확인
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Box>
+        </>
+    );
 };
 
 export default JoinBabyByCode;
