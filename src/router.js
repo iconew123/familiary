ef7aef87d8a44a30353b7135468e6e6fe702aac1
Lr2l1 <hoit5it@naver.com>
2024-05-30 00:02:19 +0900

    Add improt user

--- src/router.js
+++ src/router.js
@@ -11,6 +11,12 @@ import CommunityNotice from "./components/community/CommunityNotice";
 import CommunityRecommend from "./components/community/CommunityRecommend";
 import CreateCommunity from "./components/community/CreateCommunity";
 import UpdateCommunity from "./components/community/UpdateCommunity";
+import Join from "./components/user/Join";
+import Login from "./components/user/Login";
+import Logout from "./components/user/Logout";
+import MyPage from "./components/user/MyPage";
+import UserUpdate from "./components/user/UserUpdate"
+import UserDelete from "./components/user/UserDelete"
 
 const router = createBrowserRouter([
     {
