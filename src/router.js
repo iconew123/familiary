2ccecc6cd4d89f3dc89e9f81ae36c6b0ff3e57b1
Lr2l1 <hoit5it@naver.com>
2024-05-29 18:17:45 +0900

    Add User import

--- src/router.js
+++ src/router.js
@@ -7,55 +7,88 @@ import InfoBaby from "./components/diaryHome/baby/InfoBaby";
 import UpdateBaby from "./components/diaryHome/baby/UpdateBaby";
 import JoinBabyByCode from "./components/diaryHome/baby/JoinBabyByCode";
 
+import Join from "./components/user/Join";
+import Login from "./components/user/Login";
+import Logout from "./components/user/Logout";
+import MyPage from "./components/user/MyPage";
+import UserUpdate from "./components/user/UserUpdate"
+import UserDelete from "./components/user/UserDelete"
+
+
 const router = createBrowserRouter([
     {
-        path:"/",
+        path: "/main",
         element: <Root />,
-        children:[
+        children: [
             {
-                path:"",
+                path: "",
                 element: <Main />
             }
         ],
         errorElement: <>
-        <h1>오류 페이지입니다.</h1>
-    </>
+            <h1>오류 페이지입니다.</h1>
+        </>
     },
     {
-        path:"/diary",
+        path: "/user",
         element: <Root />,
-        children:[
+        children: [
             {
-                path:"",
-                element: <DiaryMain/>
-                
+                path: "join",
+                element: <Join />
+            },
+            {
+                path: "login",
+                element: <Login />
+            },
+            {
+                path: "logout", // Logout 경로 추가
+                element: <Logout />
+            },
+            {
+                path: "myPage",
+                element: <MyPage />,
+            },
+
+            {
+                path: "update",
+                element: <UserUpdate />
+            },
+            {
+                path: "delete",
+                element: <UserDelete />
+            },
+
+        ]
+    },
+    {
+        path: "/baby",
+        element: <Root />,
+        children: [
+            {
+                path: "/baby/update",
+                element: <UpdateBaby />
+
+            },
+            {
+                path: "/baby/info",
+                element: <InfoBaby />
+            },
+            {
+                path: "/baby/create",
+                element: <CreateBaby />
+            },
+            {
+                path: "/baby/join",
+                element: <JoinBabyByCode />
             }
         ]
     },
     {
-        path:"/baby",
-        element: <Root />,
-        children:[
-            {
-                path:"/baby/update",
-                element: <UpdateBaby/>
-                
-            },
-            {
-                path:"/baby/info",
-                element: <InfoBaby/>
-            },
-            {
-                path:"/baby/create",
-                element: <CreateBaby/>
-            },
-            {
-                path:"/baby/join",
-                element: <JoinBabyByCode/>
-            }
-        ]
+        path: "/diary",
+        element: <DiaryMain />
     },
-],{
+], {
     basename: "/familiary",
 });
 
