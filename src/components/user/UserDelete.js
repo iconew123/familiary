90cc4d8d9a4642d76905857a42fac203028468c7
Lr2l1 <hoit5it@naver.com>
2024-05-31 18:03:44 +0900

    Modify 유저 삭제, 업데이트 구현완료

--- src/components/user/UserDelete.js
+++ src/components/user/UserDelete.js
@@ -1,17 +1,18 @@
 import { Input, Button, Box, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSession } from '../module/SessionComponent'; 
+import { useSession } from '../module/SessionComponent';
 
 const UserDelete = () => {
-    const { logoutStatus } = useSession();
+    const { logoutStatus, isLoggedIn } = useSession();
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+    const [passwordError, setPasswordError] = useState('');
+    const [serverError, setServerError] = useState('');
+
     const loggedIn = sessionStorage.getItem('isLoggedIn');
     const userSample = sessionStorage.getItem('userInfo');
     const user = JSON.parse(userSample);
-    const { isLoggedIn } = useSession();
-
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -23,26 +24,43 @@ const UserDelete = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new URLSearchParams();
-        formData.append('id', user);
-        formData.append('password', password);
 
+        formData.append('id', user.id);
+        formData.append('password', password);
+        console.log(user.id);
+        console.log(user.password);
         try {
+
+            if (!password) {
+                setPasswordError('비밀번호를 입력하세요.');
+                return;
+            } else {
+                setPasswordError('');
+            }
+
+            // 비밀번호가 일치하는지 확인
+            if (password !== user.password) {
+                setPasswordError('기존 비밀번호가 일치하지 않습니다.');
+                return;
+            } else {
+                setPasswordError('');
+            }
+
             const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/User?command=delete`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-                body: formData.toString(),
-                mode: 'cors'
+                body: formData.toString()
             });
             if (response.ok) {
                 console.log('회원 탈퇴 성공');
                 logoutStatus();
-                navigate('/main');
+                navigate('/');
             } else {
                 console.log('회원 탈퇴 실패');
             }
-    
+
         } catch (error) {
             console.error('회원탈퇴 요청 중 에러 발생:', error);
         }
@@ -69,6 +87,8 @@ const UserDelete = () => {
                         size='lg' bg='white' w='100%'
                         mb={4}
                     />
+                    {serverError && <Text color="red">{serverError}</Text>}
+                    {passwordError && <Text color="red">{passwordError}</Text>}
                     <Button type="submit" w='100px' bg='#e0ccb3' marginTop='40px' _hover={{ color: '#fffbf0' }}>회원탈퇴</Button>
                 </form>
             </Box>
