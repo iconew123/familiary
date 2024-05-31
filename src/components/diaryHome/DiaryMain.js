19da12fdf3a7f7edb45ac0ef2a32c84ba9ee95b5
iconew123 <iconew123@gmail.com>
2024-05-31 18:44:34 +0900

    modify DiaryMain

--- src/components/diaryHome/DiaryMain.js
+++ src/components/diaryHome/DiaryMain.js
@@ -1,36 +1,40 @@
-import { Box, Button, Flex, Grid, GridItem, HStack, Icon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Grid, GridItem, HStack, Icon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Square, Text } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import MyCalendar from './MyCalendar';
-import { useNavigate } from 'react-router-dom';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import { EditIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { Image } from '@chakra-ui/react';
 import { CiImageOff } from "react-icons/ci";
-import { useSession } from '../module/SessionComponent';
+
+
+const fetchDiaryDetailInfo = async (formatDate) => {
+    const now = new Date();
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/diary?command=find&date=${formatDate ? formatDate : now.toDateString()}`);
+    const data = await response.json();
+    console.log("data : " + data.date);
+    return data;
+}
 
 const DiaryMain = () => {
-
     const loggedIn = sessionStorage.getItem('isLoggedIn');
     const selectedBaby = sessionStorage.getItem('isSelectedBaby');
 
-    // 세션 스토리지에서 저장된 리스트 데이터를 불러올 때
+    // 세션 스토리지에서 저장된 리스트 데이터를 불러올 때!
     const userSample = sessionStorage.getItem('userInfo');
-    const babySample = sessionStorage.getItem('babyInfo');
-
+    
     // 문자열(JSON)을 다시 리스트로 파싱하여 사용
     const user = JSON.parse(userSample);
-    const baby = JSON.parse(babySample);
-
-
+    
+    
     const navigate = useNavigate(); // React Router의 history 객체를 가져옵니다.
     const [showRecordOptions, setShowRecordOptions] = useState(false); // 기록 옵션을 표시할지 여부를 관리하는 state
     const [showSearchOptions, setShowSearchOptions] = useState(false);
 
-
     // 날짜 계산하기
     const [targetDate, setTargetDate] = useState('');
-    const [dDay, setDDay] = useState(null);
+    const [dDay, setDDay] = useState();
     const [isOpen, setIsOpen] = useState(false);
-    const [serverData, setServerData] = useState(null);
+    const [serverData, setServerData] = useState();
     const [formatDate, setFormatDate] = useState(''); // New state for selected date
 
     // 데이터 받아오기
@@ -41,52 +45,39 @@ const DiaryMain = () => {
         fetch(`${process.env.REACT_APP_SERVER_URL}/enroll?command=giveCode&user_id=${user.id}`)
             .then(response => response.json())
             .then(babyData => {
-                
-                if (!babyData.codes) {  // babyData가 비어 있는 경우 등록하기 모달을 열기
+                if (!babyData.codes) {
+                    // babyData가 비어 있는 경우 등록하기 모달을 열기
                     handleModalOpen();
-                } else {    // babyData 존재
-                    if (!selectedBaby) {    // babyData가 존재 -> baby session에 값 존재X
-                        // 모달창 열어 선택 (session 채워주기)
-                        console.log('선택된 아기X: ' + selectedBaby)
-                        setBabyData({ codes: babyData.codes, nicknames: babyData.nicknames });
-                        setIsBabyModalOpen(true);
-                    } else {
-                        // babyData 존재 -> baby 세션 존재 -> 값 불러오기
-                        console.log('선택된 아기O: ' + selectedBaby)
-                        console.log("babyCode: " + baby.code);
-                        handleClick(baby.code);
-                    }
+                } else {
+                    // babyData가 존재하는 경우 베이비 모달 열기
+                    setBabyData({ codes: babyData.codes, nicknames: babyData.nicknames });
+                    setIsBabyModalOpen(true);
                 }
             })
             .catch(error => {
                 console.error('데이터를 가져오는 중 에러 발생', error);
             });
-    }, [user.id]);
+    }, []);
 
-    const { isSelectedBaby, enrollStatus } = useSession();
+
     const [selectedBabyCode, setSelectedBabyCode] = useState(null);
-
-
-    const handleClick = async (value) => {
-
-        console.log('value: ' +value);
+     const handleClick = (value) => {
 
         setSelectedBabyCode(value);
 
-        try {
-            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/baby?command=read&baby_code=${value}&user_id=${user.id}`)
-            const data = await response.json();
-
-            setData(data);
-            setTargetDate(data.expected_date);
-
-            console.log("data(닉네임): " + data.nickname);
-            enrollStatus(data);
-            console.log(data);
-        } catch (error) {
-            console.error('데이터를 가져오는 중 에러 발생', error);
-        }
-    };
+        fetch(`${process.env.REACT_APP_SERVER_URL}/baby?command=read&code=${value}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setData(data);
+                console('sdf' + data);
+                setTargetDate(data.expected_date);
+            })
+            .catch(error => {
+                console.error('데이터를 가져오는 중 에러 발생', error);
+            });
+    }
 
 
     useEffect(() => {
@@ -104,15 +95,10 @@ const DiaryMain = () => {
 
     useEffect(() => {
         if (formatDate) {
-            fetch(`${process.env.REACT_APP_SERVER_URL}/diary?command=find&date=${formatDate}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    setServerData(data);
-                })
-                .catch(error => {
-                    console.error('데이터를 가져오는 중 에러 발생:', error);
-                });
+            console.log('formatDate : ' + formatDate);
+            fetchDiaryDetailInfo(formatDate).then(data => {
+                setServerData(data);
+            });
         }
     }, [formatDate]);
 
@@ -128,7 +114,7 @@ const DiaryMain = () => {
         if (option === 'writeDiary') {
             navigate('/daily-record');
         } else if (option === 'writeInfo') {
-            navigate('/info-record');
+            navigate('/info-record/');
         } else if (option === 'showDiary') {
             navigate('/show-diary');
         } else if (option === 'showInfo') {
@@ -156,7 +142,7 @@ const DiaryMain = () => {
         setIsOpen(false);
     };
 
-    const handleDateSelect = (date) => { // New handler for date selection
+    const handleDateSelect = (date) => {
         setFormatDate(date);
     };
 
@@ -209,8 +195,6 @@ const DiaryMain = () => {
                                 ) : (
                                     <Text fontSize="xl"></Text>
                                 )}
-
-
                                 <Button onClick={() => handleOptionClick('infoBaby')} w='100px' bg='#e0ccb3' marginTop='20px' marginRight='10px' _hover={{ color: '#fffbf0' }}>정보보기</Button>
                                 <Button onClick={handleModalOpen} w='100px' bg='#e0ccb3' marginTop='20px' _hover={{ color: '#fffbf0' }}>추가하기</Button>
 
@@ -264,7 +248,37 @@ const DiaryMain = () => {
                         </GridItem>
 
                         <GridItem w='95%' h='150px' bg='pink' area={'diaryInfo'}>
-                            상세
+                            <Grid
+                            w='100%'
+                            h='100%'
+                            templateAreas={`"babyInfoWirte" "dailyWrite"`}
+                            templateRows={'1fr 1fr'}
+                            >
+                                <GridItem bg='blue.300' area={'dailyWrite'}>
+                                    <Text fontSize='3xl'>
+                                        [일기] :
+                                        {!serverData
+                                            ? '해당일자의 정보가 없습니다'
+                                            : (serverData.date ? `[${serverData.date}]일 ` : '해당일자의 정보가 없습니다')
+                                        }
+                                        {serverData ? <Link to={`/diary/${serverData.date}`}>{serverData.title}</Link> : ""}
+                                    </Text>
+                                </GridItem>
+                                <GridItem bg='green' area={'babyInfoWirte'}>
+                                    <Flex color='white' textAlign='center'>
+                                        <Center w='auto' h='auto'>
+                                            <Text> [일기] </Text>
+                                        </Center>
+                                        <Square size='200px'>
+                                            <Text>formatDate</Text>
+                                        </Square>
+                                        <Center flex='1' bg='tomato' textAlign='center'>
+                                            <Text>내용</Text>
+                                        </Center>
+                                    </Flex>
+                                </GridItem>
+                            </Grid>
+
                         </GridItem>
 
                         <GridItem w='95%' area={'select'}>
@@ -297,4 +311,5 @@ const DiaryMain = () => {
     );
 };
 
+export {fetchDiaryDetailInfo};
 export default DiaryMain;
