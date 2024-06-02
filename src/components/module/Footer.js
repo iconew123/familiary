9c94f33619aad0c838128ea4510b9fddb5e5d3a2
yoon-wish <sowon5685@gmail.com>
2024-06-02 21:14:51 +0900

    Add Footer

--- src/components/module/Footer.js
+++ src/components/module/Footer.js
@@ -1,11 +1,21 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Link, Text } from '@chakra-ui/react';
 import React from 'react';
+const fontFamily = { fontFamily: "'Nanum Gothic', cursive" };
 
 const Footer = () => {
     return (
         <>
             <Box>
-                <address>서울시 서초구</address>
+                <Box>
+                <Flex justify="center" bg='#F2DBC2' h='30px' marginTop='15px'>
+                    <Link fontFamily="'Nanum Gothic', cursive">광고&제휴 문의</Link>ㅤ|ㅤ
+                    <Link fontFamily="'Nanum Gothic', cursive"> 회사소개</Link>ㅤ|ㅤ
+                    <Link fontFamily="'Nanum Gothic', cursive"> 이용약관</Link>ㅤ|ㅤ
+                    <Link fontFamily="'Nanum Gothic', cursive"> 개인정보처리방침</Link>
+
+                </Flex>
+                </Box>
+                <Text textAlign='center' fontFamily="'Nanum Gothic', cursive">(주)삼김일윤ㅤ|ㅤ대표 : 김재현</Text>
             </Box>
         </>
     );
