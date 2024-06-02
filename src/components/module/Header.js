f26796c70976ff7173811c118b42648aa1298719
ksy7615 <sykim7615@daum.net>
2024-06-03 00:10:28 +0900

    Modify: Header community

--- src/components/module/Header.js
+++ src/components/module/Header.js
@@ -4,12 +4,9 @@ import { Text } from '@chakra-ui/react'
 import { Link, useLocation } from 'react-router-dom';
 <link href="https://fonts.googleapis.com/css2?family=Gaegu&display=swap" rel="stylesheet"></link>
 
-
-
 const Header = () => {
     const location = useLocation();
     const isDiaryPage = location.pathname === '/diary';
-    const isCommunityPage = location.pathname === '/community';
     const loggedIn = sessionStorage.getItem('isLoggedIn');
 
     // 세션 스토리지에서 저장된 리스트 데이터를 불러올 때
@@ -18,8 +15,31 @@ const Header = () => {
     // 문자열(JSON)을 다시 리스트로 파싱하여 사용
     const user = JSON.parse(userSample);
 
-
     const fontFamily = { fontFamily: "'Nanum Gothic', cursive" };
+
+
+    const handleSelectChange = (e) => {
+        const selectedOption = e.target.value;
+        let url = '';
+
+        switch (selectedOption) {
+            case 'notice':
+                url = 'http://localhost:3000/familiary/community/notice?command=read/notice';
+                break;
+            case 'chat':
+                url = 'http://localhost:3000/familiary/community/chat?command=read/chat';
+                break;
+            case 'recommend':
+                url = 'http://localhost:3000/familiary/community/recommend?command=read/recommend';
+                break;
+            default:
+                break;
+        }
+
+        if (url) {
+            window.location.href = url;
+        }
+    };
 
     return (
         <>
@@ -35,21 +55,27 @@ const Header = () => {
                     )}
                 </Box>
 
-                <Box>
-                    <Text>ㅤ</Text>
-                </Box>
-
                 <Box align='center'>
                     <Link to='/'><Text marginLeft='100px' fontSize='5xl' as='b' color='#765d2f' >Familiary</Text>
                     </Link>
                     <nav>
                         <Flex justify="center" bg='#e0ccb3' h='48px' marginTop='15px'>
                             <Link to={loggedIn ? '/diary' : '/user/login'}>
-                                <Text fontSize='xl' marginTop='8px' marginRight='20px' color={isDiaryPage ? '#fff' : '#765d2f'}  fontFamily="'Nanum Gothic', cursive">DIARY</Text>
+                                <Text fontSize='xl' marginTop='8px' marginRight='20px' color={isDiaryPage ? '#fff' : '#765d2f'} fontFamily="'Nanum Gothic', cursive">DIARY</Text>
                             </Link>
-                            <Link to='/community'>
-                                <Text fontSize='xl' marginTop='8px' marginRight='20px' color={isCommunityPage ? '#fff' : '#765d2f'} fontFamily="'Nanum Gothic', cursive">COMMUNITY</Text>
-                            </Link>
+                            <Select
+                                w='150px'
+                                size='lg'
+                                variant='unstyled'
+                                marginTop='11px'
+                                fontFamily="'Nanum Gothic', cursive"
+                                onChange={handleSelectChange}
+                            >
+                                <option>COMMUNITY</option>
+                                <option value="notice">공지사항</option>
+                                <option value="chat">자유 게시판</option>
+                                <option value="recommend">추천 게시판</option>
+                            </Select>
                             <Select placeholder='INFO' w='80px' size='lg' variant='unstyled' marginTop='11px' fontFamily="'Nanum Gothic', cursive">
                                 <option value='option1' >임산부 가이드</option>
                                 <option value='option2'>정부 지원사업</option>
@@ -64,7 +90,6 @@ const Header = () => {
                 </Box>
             </Box>
         </>
-
 
     );
 };
